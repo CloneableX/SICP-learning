438e7ab3e442ddb7cdb582a4f6566bdd1d03ef6b
Cloneable <cloneable@163.com>
2021-03-28 19:23:47 +0800

    remove exercise 2.6

--- 2.1/exercises/2-6.scm
+++
@@ -1,9 +0,0 @@
-(define one (lambda (f) (lambda (x) (f x))))
-
-(define two (lambda (f) (lambda (x) (f (f x)))))
-
-(define + (lambda (m)
-	    (lambda (n)
-	      (lambda (f)
-		(lambda (x)
-		  (m f (n f x)))))))
